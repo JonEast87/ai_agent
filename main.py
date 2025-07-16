53ca038dc2d670dc6fe8b8314dbdb0fc3b978255
Jon Eastin <picardncce@gmail.com>
2025-07-16 09:56:05 -0700

    adding a list to track the correspondence between the user and agent. added a verbose comman d

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
 try:
-    question = sys.argv[1]
+    user_prompt = sys.argv[1]
 except:
     print("No question provided")
     exit(1)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=question)
-print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens: ", response.usage_metadata.candidates_token_count)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+try:
+    if sys.argv[2] == "--verbose":
+        print("User prompt: {}".format(sys.argv[1]))
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+except:
+    print(response.text)
